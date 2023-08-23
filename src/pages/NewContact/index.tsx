a31f10f3eb31fbd8fff96fccc83eeb94017eb692
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-08-23 14:13:16 -0300

    navigation bar on new contact page

--- src/pages/NewContact/index.tsx
+++ src/pages/NewContact/index.tsx
@@ -1,9 +1,11 @@
 import Form from '../../components/Form';
 import Header from '../../components/Header';
+import NavigationBar from '../../components/NavigationBar';
 
 const NewContact = () => (
   <>
     <Header />
+    <NavigationBar />
     <Form />
   </>
 );
